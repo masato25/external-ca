2c6aef4d9756feae67e14ebde91833678ee64b1b
masato25 <leomasato25@gmail.com>
2021-04-29 12:23:08 +0800

    fix: fix typo variable name

--- scripts/rename_orderer_org.sh
+++ scripts/rename_orderer_org.sh
@@ -19,11 +19,11 @@ echo "Current os: $machine"
 if [ $machine = "Linux" ]
 then
   find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i "s/$CURRENTORGDOMAIN/$REPLACEORGDOMAIN/g" {} \;
-  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i "s/$CURRENTORGDOMAIN/$REPLACEORGMSPNAME/g" {} \;
+  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i "s/$CURRENTORGMSPNAME/$REPLACEORGMSPNAME/g" {} \;
 elif [ $machine = "Mac" ]
 then
   find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i '' -e "s/$CURRENTORGDOMAIN/$REPLACEORGDOMAIN/g" {} \;
-  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i '' -e "s/$CURRENTORGDOMAIN/$REPLACEORGMSPNAME/g" {} \;
+  find ./ ! -iname 'rename*.sh' -type f \( -iname \*.yaml -o -iname \*.sh -o -iname \*.cnf -o -iname \*.md \) -exec sed -i '' -e "s/$CURRENTORGMSPNAME/$REPLACEORGMSPNAME/g" {} \;
 else
   echo "$machine is unsupported!"
 fi
